c717e1839cd3b8d97894270b4d76eedd0da0cae9
João Vitor B. <jv23072006@gmail.com>
2025-01-16 13:21:12 +0000

    Aula11

---
+++ Exercises/list11_OOP/list11_Q1/Program.cs
@@ -0,0 +1,37 @@
+﻿CalcUI.MainUI();
+
+static class CalcUI
+{
+    public static void MainUI(){
+        int value = 0;
+
+        do{
+            try
+            {
+                value = Menu();
+
+                switch(value)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        CalcArea();
+                        break;
+                    default:
+                        Console.WriteLine("Valor Inválido!");
+                        break;
+                }
+
+            } catch {
+                Console.WriteLine("Valor Inválido!");
+            }
+
+        } while(value != 0);
+    }
+
+    public static int Menu(){
+        Console.WriteLine("--- Calcular Área e Diagonal ---");
+        Console.WriteLine("Informe [1]Entrar [0]Sair");
+        return int.Parse(Console.ReadLine());
+    } 
+}
